31764e9f6df00abc9613e3c9afbedb0add87d360
DFCU\stuart.smith <stuarts205@gmail.com>
2025-02-10 15:08:31 -0700

    super json

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,18 +1,20 @@
 import { z } from 'zod';
-import { baseProcedure, createTRPCRouter } from '../init';
-import { TRPCClientError } from '@trpc/client';
+import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
+import { auth } from '@clerk/nextjs/server';
+
 export const appRouter = createTRPCRouter({
-  hello: baseProcedure
+  hello: protectedProcedure
     .input(
       z.object({
         text: z.string(),
       }),
     )
     .query((opts) => {
+      console.log({ dbUser: opts.ctx.user });
       return {
         greeting: `hello ${opts.input.text}`,
       };
     }),
 });
-// export type definition of API
+
 export type AppRouter = typeof appRouter;
