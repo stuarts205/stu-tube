8a22839315d0e6c61aef4c9b799b14f1bab3094a
DFCU\stuart.smith <stuarts205@gmail.com>
2025-02-13 10:43:05 -0700

    video content page

---
+++ src/components/infinite-scroll.tsx
@@ -0,0 +1,54 @@
+"use client";
+import { useIntersectionObserver } from "@/hooks/use-intersection-observer";
+import React, { use, useEffect } from "react";
+import { Button } from "@/components/ui/button";
+
+interface InfiniteScrollProps {
+  isManual?: boolean;
+  hasNextPage: boolean;
+  isFetchingNextPage: boolean;
+  fetchNextPage: () => void;
+}
+
+const InfiniteScroll = ({
+  isManual = false,
+  hasNextPage,
+  isFetchingNextPage,
+  fetchNextPage,
+}: InfiniteScrollProps) => {
+  const { targetRef, isIntersecting } = useIntersectionObserver({
+    threshold: 0.5,
+    rootMargin: "100px",
+  });
+
+  useEffect(() => {
+    if (isIntersecting && hasNextPage && !isFetchingNextPage && !isManual) {
+      fetchNextPage();
+    }
+  }, [
+    isIntersecting,
+    hasNextPage,
+    isFetchingNextPage,
+    fetchNextPage,
+    isManual,
+  ]);
+
+  return (
+    <div className="flex flex-col items-center gap-4 p-4">
+      <div ref={targetRef} className="h-1" />
+      {hasNextPage ? (
+        <Button
+          variant="secondary"
+          onClick={() => fetchNextPage()}
+          disabled={isFetchingNextPage || !hasNextPage}
+        >
+          {isFetchingNextPage ? "Loading..." : "Load More"}
+        </Button>
+      ) : (
+        <p>You have reached the end of list</p>
+      )}
+    </div>
+  );
+};
+
+export default InfiniteScroll;
--- src/modules/studio/ui/sections/videos-sections.tsx
+++ src/modules/studio/ui/sections/videos-sections.tsx
@@ -1,17 +1,78 @@
-'use client'
-import { DEFAULT_LIMIT } from '@/constants'
-import { trpc } from '@/trpc/client'
-import React from 'react'
+"use client";
+import { DEFAULT_LIMIT } from "@/constants";
+import { trpc } from "@/trpc/client";
+import { ErrorBoundary } from "react-error-boundary";
+import React, { Suspense } from "react";
+import InfiniteScroll from "@/components/infinite-scroll";
+import { 
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import Link from "next/link";
 
-const VideosSection = () => {
-  const [data] = trpc.studio.getMany.useSuspenseInfiniteQuery({
-    limit: DEFAULT_LIMIT,
-  }, {
-    getNextPageParam: (lastPage) => lastPage.nextCursor
-  })
+export const VideosSection = () => {
   return (
-    <div>{JSON.stringify(data)}</div>
-  )
-}
+    <Suspense fallback={<p>Loading...</p>}>
+      <ErrorBoundary fallback={<p>Error</p>}>
+        <VideosSectionSuspense />
+      </ErrorBoundary>
+    </Suspense>
+  );
+};
 
-export default VideosSection
+const VideosSectionSuspense = () => {
+  const [videos, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
+    {
+      limit: DEFAULT_LIMIT,
+    },
+    {
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    }
+  );
+  return (
+    <div>
+      <div className="border-y">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="pl-6 w-[510px]">Video</TableHead>
+              <TableHead>Visibilty</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="text-right">Views</TableHead>
+              <TableHead className="text-right">Comments</TableHead>
+              <TableHead className="text-right pr-6">Likes</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {videos.pages.flatMap((page) => page.items).map((video) => (
+              <Link href={`/studio/videos/${video.id}`} key={video.id} legacyBehavior>
+                <TableRow className="cursor-pointer">
+                  <TableCell>{video.title}</TableCell>
+                  <TableCell>vis</TableCell>
+                  <TableCell>status</TableCell>
+                  <TableCell>date</TableCell>
+                  <TableCell className="text-right">views</TableCell>
+                  <TableCell className="text-right">comments</TableCell>
+                  <TableCell className="text-right">likes</TableCell>
+                </TableRow>
+              </Link>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+      <InfiniteScroll
+        isManual
+        hasNextPage={query.hasNextPage}
+        isFetchingNextPage={query.isFetchingNextPage}
+        fetchNextPage={query.fetchNextPage}
+      />
+    </div>
+  );
+};
+
+export default VideosSection;
