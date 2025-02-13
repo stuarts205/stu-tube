48d3ea4773c2199769d7e457a330c48eb379cb23
DFCU\stuart.smith <stuarts205@gmail.com>
2025-02-13 13:21:32 -0700

    mux intergration

---
+++ src/app/api/videos/webhook/route.ts
@@ -0,0 +1,63 @@
+import { eq } from "drizzle-orm";
+import {
+  VideoAssetCreatedWebhookEvent,
+  VideoAssetErroredWebhookEvent,
+  VideoAssetReadyWebhookEvent,
+  VideoAssetTrackReadyWebhookEvent,
+} from "@mux/mux-node/resources/webhooks";
+import { headers } from "next/headers";
+import { mux } from "@/lib/mux";
+import { db } from "@/db";
+import { videos } from "@/db/schema";
+
+const SIGNING_SECRET = process.env.MUX_WEBHOOK_SECRET!;
+
+type WebhookEvent =
+  | VideoAssetCreatedWebhookEvent
+  | VideoAssetErroredWebhookEvent
+  | VideoAssetReadyWebhookEvent
+  | VideoAssetTrackReadyWebhookEvent;
+
+export const POST = async (request: Request) => {
+  if (!SIGNING_SECRET) {
+    throw new Error("No MUX signing secret");
+  }
+
+  const headersPayload = await headers();
+  const muxSignature = headersPayload.get("mux-signature");
+
+  if (!muxSignature) {
+    throw new Response("No MUX signature", { status: 401 });
+  }
+
+  const payload = await request.json();
+  const body = JSON.stringify(payload);
+
+  mux.webhooks.verifySignature(
+    body,
+    {
+      'mux-signature': muxSignature,
+    },
+    SIGNING_SECRET
+  );
+
+  switch (payload.type as WebhookEvent["type"]) {
+    case "video.asset.created": {
+      const data = payload.data as VideoAssetCreatedWebhookEvent["data"];
+      if (!data.upload_id) {
+        throw new Response("No upload id in webhook data", { status: 400 });
+      }
+
+      await db
+        .update(videos)
+        .set({
+          muxAssetId: data.id,
+          muxStatus: data.status,
+        })
+        .where(eq(videos.muxUploadId, data.upload_id));
+      break;
+    }
+  }
+
+  return new Response("Webhook received", { status: 200 });
+};
--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -1,5 +1,6 @@
 import { db } from "@/db";
 import { videos } from "@/db/schema";
+import { mux } from "@/lib/mux";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { TRPCError } from "@trpc/server";
 
@@ -7,13 +8,30 @@ export const videosRouter = createTRPCRouter({
   create: protectedProcedure.mutation(async ({ ctx }) => {
     const { id: userId } = ctx.user;
 
+    const upload = await mux.video.uploads.create({
+      new_asset_settings: {
+        passthrough: userId,
+        playback_policy: ["public"],       
+      },
+      cors_origin: "*",
+    })
+
+    console.log({upload})
+    console.log({url: upload.url})
+
     const [video] = await db
       .insert(videos)
-      .values({ userId, title: "Untitled" })
+      .values({ 
+        userId, 
+        title: "Untitled",
+        muxStatus: 'waiting', 
+        muxUploadId: upload.id,
+      })
       .returning();
 
     return {
       video: video,
+      url: upload.url,
     };
   }),
 });
